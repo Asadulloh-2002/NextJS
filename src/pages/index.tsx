ba09a64ee142495a334d45d59e48189bdb1d5d42
Asadulloh-2002 <asadullohusanboyev56@gmail.com>
2023-10-03 11:21:06 +0500

    fix/remove component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { Box, Button } from '@mui/material'
 import React from 'react'
 import Layout from '../layout/Layout'
-import { Content, Hero, SideBar } from '../components'
+import { Hero, SideBar } from '../components'
 
 const Index = () => {
   return (
@@ -9,7 +9,6 @@ const Index = () => {
       <Hero />
       <Box sx={{ display: 'flex', gap: '20px', padding: '20px' }}>
         <SideBar />
-        <Content />
       </Box>
     </Layout>
   )
